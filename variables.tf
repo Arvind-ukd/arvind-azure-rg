8ccbe6fa76828ba42f11dc3cbdf08451db1f75e7
arvindng88 <arvindng88@gmail.com>
2022-09-13 17:54:29 +0530

    India

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "location" {
   description = "Which location (Central India, South India, West US, East US)"
-  default     = "Central India, South India, West US, East US"
+  default     = "Central India"
 }

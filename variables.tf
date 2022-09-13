56830cb2a98a6fdba0617e730e9a803c3f989d77
arvindng88 <arvindng88@gmail.com>
2022-09-13 18:08:49 +0530

    atnat

--- variables.tf
+++ variables.tf
@@ -1,4 +1,4 @@
 variable "location" {
   description = "Which location (Central India, South India, West US, East US)"
-  default     = ["Central India", "East US", "South India", "West US"]
+  default     = ["Central", "East", "South", "West"]
 }

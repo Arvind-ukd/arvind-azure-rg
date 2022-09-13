59a1dac070eab3be4bbf7d5441a93be2803c8779
arvindng88 <arvindng88@gmail.com>
2022-09-13 17:22:39 +0530

    location

---
+++ main.tf
@@ -0,0 +1,4 @@
+resource "azurerm_resource_group" "example" {
+  name     = "arvind-rg-13sept2022"
+  location = "${var.location}"
+}
---
+++ vartiable.tf
@@ -0,0 +1,4 @@
+variable "location" {
+  description = "Which location (Central India, South India, West US, East US)"
+  default     = "East US"
+}
